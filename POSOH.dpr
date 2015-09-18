d23b1492d628baedd41b600c55494e2d7f5a71fa
CountVine <count.vine@gmail.com>
2015-09-19 00:45:22 +0300

    Empty project

---
+++ POSOH.dpr
@@ -0,0 +1,14 @@
+program POSOH;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  main in 'main.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
